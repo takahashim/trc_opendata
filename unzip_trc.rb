1d56bec8036a374a929ab4209f08bcb6d0fe4c71
takahashim <maki@rubycolor.org>
2017-08-27 14:57:57 +0900

    add origianl .txt, support header of .tsv

--- unzip_trc.rb
+++ unzip_trc.rb
@@ -6,6 +6,7 @@ require 'csv'
 require 'json'
 
 JSON_COLS = %w[isbn	title	subtitle	author	author2	edition	publisher	publisher2	publishing_date	page	size	product_part	series	series2	series3	volume_title	price	set_price]
+TSV_COLS = "ISBN	タイトル	サブタイトル	著者	著者2	版表示	出版社	発売者	出版年月	ページ数等	大きさ	付属資料の種類と形態	シリーズ名・シリーズ番号	シリーズ名・シリーズ番号2	シリーズ名・シリーズ番号3	各巻のタイトル	本体価格	セット本体価格"
 
 DATA_DIR = "docs"
 
@@ -14,14 +15,18 @@ def rename_ext(path, ext)
 end
 
 def unzip_tsv(file)
-  tsv_path = nil
+  txt_path = nil
   Zip::InputStream.open(file) do |zis|
     entry = zis.get_next_entry
     txt_path = File.join(DATA_DIR, entry.name)
+    content = zis.read
+    content.force_encoding("utf-8")
+    File.write(txt_path, content)
     tsv_path = rename_ext(txt_path, ".tsv")
-    File.write(tsv_path, zis.read)
+    tsv_data = TSV_COLS + "\r\n" + content
+    File.write(tsv_path, tsv_data)
   end
-  tsv_path
+  txt_path
 end
 
 def tsv2json(file)
