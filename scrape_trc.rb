71369b3fc307a81d7332a5618013959682481e64
takahashim <maki@rubycolor.org>
2017-08-27 13:03:09 +0900

    refactor methods

--- scrape_trc.rb
+++ scrape_trc.rb
@@ -4,23 +4,38 @@ require 'open-uri'
 require 'nokogiri'
 require 'uri'
 
-url = "http://www.trc.co.jp/trc_opendata/"
 ZIP_DIR = "zips"
 
-charset = nil
-html = open(url) do |f|
-  charset = f.charset
-  f.read
+def download_index_page(url)
+  charset = nil
+  html = open(url) do |f|
+    charset = f.charset
+    f.read
+  end
+
+  page = Nokogiri::HTML.parse(html, nil, charset)
+  page
 end
 
-page = Nokogiri::HTML.parse(html, nil, charset)
-page.css("section a").each do |link|
-  href = link["href"]
-  filename = File.basename(href)
-  zip_filename = File.join(ZIP_DIR, filename)
-  if filename =~ /.zip$/ && !File.exists?(zip_filename)
-    zip_url = URI.join(url, href)
-    zip_content = open(zip_url).read
-    File.write(zip_filename, zip_content)
+def find_zip_path(page, url)
+  page.css("section a").each do |link|
+    href = link["href"]
+    filename = File.basename(href)
+    zip_path = File.join(ZIP_DIR, filename)
+    if filename =~ /.zip$/ && !File.exists?(zip_path)
+      zip_url = URI.join(url, href)
+      yield zip_url, zip_path
+    end
   end
 end
+
+def main
+  url = "http://www.trc.co.jp/trc_opendata/"
+  page = download_index_page(url)
+  find_zip_path(page, url) do |zip_url, path|
+    zip_content = open(zip_url).read
+    File.write(path, zip_content)
+  end
+end
+
+main
